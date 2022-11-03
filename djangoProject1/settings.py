b1b2598baaa2fc86d03208ba3e7a2eb1fae8cb58
jangbongjoon <lollypark501@naver.com>
2022-11-04 01:25:25 +0900

    runserver 수정

--- djangoProject1/settings.py
+++ djangoProject1/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'common.apps.CommonConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -38,8 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'user',
-    'housecordi',
-    'mysite',
+
 ]
 
 MIDDLEWARE = [
